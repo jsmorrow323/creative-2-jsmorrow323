8fcf585243500c9c2f7d20dfe6f96cb73e724a4a
EC2 Default User <ec2-user@ip-172-31-8-48.us-east-2.compute.internal>
2018-10-05 01:40:09 +0000

    Functions

--- code.js
+++ code.js
@@ -1,18 +1,68 @@
-
 function synonymSearch(e) {
-
-    var myurl = "";
+    var value = document.getElementById("synonym").value;
+    var myurl =  "https://wordsapiv1.p.mashape.com/words/" + value + "/synonyms"
     
     $.ajax({
         url: myurl,
         dataType: "json",
         cache: false,
+        beforeSend: function(xhr) {
+            xhr.setRequestHeader("X-Mashape-Key", "UNadWDxFEcmshuJMJAABzA8mGoS8p1FP2mUjsnlaCBpcgmGUgd")
+            xhr.setRequestHeader("X-Mashape-Host", "wordsapiv1.p.mashape.com")
+        } ,
         success: function(parsed_json) {
-            var everything = "";
-            for (var i = 0; i < parsed_json.items.length; ++i) {
-                
+            var everything = "Results: ";
+            for (var i = 0; i < parsed_json.synonyms.length; ++i) {
+                var synonym = parsed_json.synonyms[i];
+                if (!synonym.indexOf(synonym)) {
+                    everything += "<ul>"
+                    everything += "<li  onclick='showDefinition(synonym)'>" + synonym; + "</li>"
+                    everything += "</ul>";
+                }
             }
             $("#search").html(everything);
         }
     });
+}
+
+function buttonClick(e) {
+    if (event.keyCode == 13) {
+    document.getElementById('button').click()
+    event.returnValue=false;
+    event.cancel=true;
+    }
+}
+
+function showDefinition(synonym) {
+    var listItems = document.querySelectorAll("ul li");
+    var word = "";
+    listItems.forEach(function(item) {
+        item.onclick = function(e) {
+        word = (this.innerText);
+        
+    var myurl =  "https://wordsapiv1.p.mashape.com/words/" + word + "/definitions"
+    
+    $.ajax({
+        url: myurl,
+        dataType: "json",
+        cache: false,
+        beforeSend: function(xhr) {
+            xhr.setRequestHeader("X-Mashape-Key", "UNadWDxFEcmshuJMJAABzA8mGoS8p1FP2mUjsnlaCBpcgmGUgd")
+            xhr.setRequestHeader("X-Mashape-Host", "wordsapiv1.p.mashape.com")
+        } ,
+        success: function(parsed_json) {
+            var everything = "Definitions: ";
+            for (var i = 0; i < parsed_json.definitions.length; ++i) {
+                var definition = parsed_json.definitions[i].definition;
+                
+                everything += "<ul>"
+                everything += "<li>" + [i + 1] + ". "  + definition + "." + "</li>"
+                everything += "</ul>";
+            }
+            $("#definition").html(everything);
+        }
+    });    
+        
+  }
+});
 }
